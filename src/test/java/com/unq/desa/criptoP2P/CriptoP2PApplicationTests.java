55f112c2e5f61d179fe792d75980d3a5a21aed9e
AleGareca <agareca@vates.com>
2022-08-28 18:18:59 -0300

    init

---
+++ src/test/java/com/unq/desa/criptoP2P/CriptoP2PApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unq.desa.criptoP2P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoP2PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
