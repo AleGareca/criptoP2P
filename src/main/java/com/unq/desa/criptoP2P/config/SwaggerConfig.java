ef1b4941c3204bebaf40fc657b9e63445f135ebe
Ale <gereca20jorge@gmail.com>
2022-09-26 20:16:36 -0300

    add swagger

--- src/main/java/com/unq/desa/criptoP2P/config/SwaggerConfig.java
+++ src/main/java/com/unq/desa/criptoP2P/config/SwaggerConfig.java
@@ -4,24 +4,35 @@ package com.unq.desa.criptoP2P.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.unq.desa.criptoP2P"))
+                .paths(regex("/.*"))
                 .build();
+    }
+    @Bean
+    UiConfiguration uiConfiguration(){
+        return UiConfigurationBuilder.builder().displayRequestDuration(true).validatorUrl(null).build();
     }
 
 
@@ -29,9 +40,10 @@ public class SwaggerConfig {
         return new ApiInfoBuilder().title("Desarrollo de aplicaciones: Universidad Nacional de Quilmes")
                 .description("Tecnologia de codigo abierto (Spring)").build();
     }
-
-    public void addResouceHandler(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resouces");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
