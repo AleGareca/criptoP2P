7b2d819a98dbede2c8c5556bc15693af5e7276dd
Ale <gereca20jorge@gmail.com>
2022-10-19 21:07:52 -0300

    Se cambio la dependencia de FeigCLient

--- src/main/java/com/unq/desa/criptoP2P/CriptoP2PApplication.java
+++ src/main/java/com/unq/desa/criptoP2P/CriptoP2PApplication.java
@@ -3,10 +3,12 @@ package com.unq.desa.criptoP2P;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @EnableFeignClients
+@EnableAsync
 public class CriptoP2PApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/unq/desa/criptoP2P/client/BinanceClient.java
+++ src/main/java/com/unq/desa/criptoP2P/client/BinanceClient.java
@@ -1,13 +1,13 @@
 package com.unq.desa.criptoP2P.client;
 
 import com.unq.desa.criptoP2P.model.cryptoCurrency.Cryptocurrency;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient(url="${client.post.baseUrl}")
+@FeignClient(value="BinanceClient",url="${client.post.baseUrl}")
 public interface BinanceClient {
     @RequestMapping(method = RequestMethod.GET, value = "/price?symbol=")
     Cryptocurrency getCryptocurrency(String cryptocurrency);
